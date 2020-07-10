af44863b08049092b42568b8ab0c225180288c19
Doug Owings <doug@dougowings.net>
2020-07-10 16:44:06 -0700

    read device response example

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 console.log('hello world')
 
 const SerialPort = require('serialport')
+const Readline = require('@serialport/parser-readline')
 
-var mc = new SerialPort(process.env.MC_SERIAL_PORT, {
+const mc = new SerialPort(process.env.MC_SERIAL_PORT, {
     baudRate: 115200,
     autoOpen: false
 })
+
+const parser_mc = mc.pipe(new Readline)
 
 mc.open(err => {
     if (err) {
@@ -14,7 +17,14 @@ mc.open(err => {
     }
     console.log('Opened, delaying 2 seconds')
     setTimeout(() => {
-        mc.write(':01 1 1 1600;')
-        mc.close()
+        console.log('Writing command')
+        parser_mc.once('data', resText => {
+            console.log('Received response:', resText)
+        })
+        mc.write(':01 1 1 1600;', () => {
+            setTimeout(() => mc.close(), 2000)
+            //mc.close()
+        })
+        
     }, 2000)
 })
