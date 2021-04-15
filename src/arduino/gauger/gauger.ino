770a27ca7be53d9ee5aad734c59bda93c9299e2a
Doug Owings <doug@dougowings.net>
2021-04-15 01:15:35 -0700

    add magnetometer to gauger

--- src/arduino/gauger/gauger.ino
+++ src/arduino/gauger/gauger.ino
@@ -6,6 +6,8 @@
  *  :<id>:01 <mode>;
  *
  */
+#include <Adafruit_Sensor.h>
+#include <Adafruit_HMC5883_U.h>
 #include <SoftwareSerial.h> 
 #include <TinyGPS.h>
 
@@ -16,14 +18,30 @@
 #define maxMode 3
 byte mode = 1;
 
+// GPS
 SoftwareSerial gpsSerial(8,9); //rx, tx
 TinyGPS gps;
 float gps_lat = -1;
 float gps_lon = -1;
 
+
+// Mag
+
+/* Assign a unique ID to this sensor */
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(49138);
+float declinationAngle = 0.23; // radians
+boolean isMagInit = false;
+float mag_x = 0; // micro-Tesla (uT)
+float mag_y = 0;
+float mag_z = 0;
+float mag_heading = -1; // degrees
+
 void setup() {
   Serial.begin(9600);
   gpsSerial.begin(9600);
+  if (mag.begin()) {
+    isMagInit = true;
+  }
 }
 
 void loop() {
@@ -84,18 +102,32 @@ void takeCommand(Stream &input, Stream &output) {
 
 void writeAll(Stream &output) {
   output.write("GPS:");
-  writeLatLong(output);
+  writeGps(output);
+  output.write("\n");
+  output.write("MAG:");
+  writeMag(output);
   output.write("\n");
 }
 
-void writeLatLong(Stream &output) {
+void writeGps(Stream &output) {
   output.print(gps_lat, 6);
   output.write('|');
   output.print(gps_lon, 6);
 }
 
+void writeMag(Stream &output) {
+  output.print(mag_heading, 4);
+  output.write('|');
+  output.print(mag_x, 4);
+  output.write('|');
+  output.print(mag_y, 4);
+  output.write('|');
+  output.print(mag_z, 4);
+}
+
 void readAll() {
   readGps();
+  readMag();
 }
 
 void readGps() {
@@ -110,4 +142,32 @@ void streamGps(Stream &output) {
   while (gpsSerial.available()) {
     output.write(gpsSerial.read());
   }
+}
+
+void readMag() {
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  // magnetic vector values are in micro-Tesla (uT)
+  mag_x = event.magnetic.x;
+  mag_y = event.magnetic.y;
+  mag_z = event.magnetic.z;
+  
+  // Hold the module so that Z is pointing 'up' and you can measure the heading with x&y
+  // Calculate heading when the magnetometer is level, then correct for signs of axis.
+  float heading = atan2(event.magnetic.y, event.magnetic.x);
+
+  heading += declinationAngle;
+  
+  // Correct for when signs are reversed.
+  if(heading < 0) {
+    heading += 2*PI;
+  }
+
+  // Check for wrap due to addition of declination.
+  if(heading > 2*PI) {
+    heading -= 2*PI;
+  }
+   
+  // Convert radians to degrees for readability.
+  mag_heading = heading * 180/M_PI; 
 }
