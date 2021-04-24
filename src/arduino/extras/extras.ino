a5c3b3d44ec0aa906dd0532c4be25fd26697c135
Doug Owings <doug@lab5.co>
2021-04-24 11:56:51 -0700

    update i2c

--- src/arduino/extras/extras.ino
+++ src/arduino/extras/extras.ino
@@ -1,4 +1,4 @@
-
+// this is a dump of various deleted code, mostly from motor-controller
 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
--- src/pi/lcd/ri.py
+++ src/pi/lcd/ri.py
@@ -1,8 +1,9 @@
 import I2C_LCD_driver
 import time
 from RPi import GPIO
+I2C_LCD_driver.I2CBUS = 1
 
-lcd = I2C_LCD_driver.lcd()
+lcd = I2C_LCD_driver.lcd(0x3f)
 
 lcd.lcd_display_string("Hello World!", 1)
 lcd.lcd_display_string("Bonjour", 2)
@@ -16,6 +17,8 @@ lcd.backlight(0)
 time.sleep(1)
 
 lcd.backlight(1)
+
+
 
 # rotary encoder
 #  see https://github.com/modmypi/Rotary-Encoder/blob/master/rotary_encoder.py
